58dd7d3fca3ec4c865b370fbf20dd91f5c6beb99
hamt3ch <hmiles23@ufl.edu>
2015-03-27 00:46:55 -0400

    finshed diff21

--- codingBat.py
+++ codingBat.py
@@ -92,6 +92,21 @@ def near_hundred(n):
 		return False
 """
 
+Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21. 
+
+diff21(19) → 2
+diff21(10) → 11
+diff21(21) → 0
+"""
+def diff21(n):
+  if abs(n) < 21
+  	return abs(21 - n)
+  else:
+  	return 2*n
+
+
+"""
+NOT DONE
 Given 2 int values, return True if one is negative and one is positive. Except if the parameter "negative" is True, then return True only if both are negative. 
 
 pos_neg(1, -1, False) → True
