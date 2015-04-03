29115dc85597166239b020199f9d09cf361382bf
Hugh A. Miles II <hmiles@lin113-21.cise.ufl.edu>
2015-04-02 21:09:05 -0400

    From computerLab

--- gardenHull.py
+++ gardenHull.py
@@ -1,12 +1,49 @@
 from sys import stdin
+import math
 
 file = open('gardenIn.txt', 'r')
 
-numberOfPoints = file.readline()
+numberOfPoints = int(file.readline()) #Parse inputs
+points = []
+for ele in range(0,numberOfPoints):
+	x,y = file.readline().strip().split()
+	points.append([int(x),int(y)])
 
-points = [] 
+points.sort() # least to greatest
+start = points[0]
+trip = [start]
 
-for x,y in file.readlines():
-	points.append([x,y])
+for i in range(0,len(points)):
+	x1 = points[i][0]
+	y1 = points[i][1]
+	x2 = points[i+1][0]
+	y2 = points[i+1][1]	
 
-print(numberOfPoints)
+	if x1 == x2:
+		slope = y2 - y1 #distance for between y
+	else:
+		slope = (y2-y1)/(x2-x1) #distance between points
+
+	if slope > 0: # leftTurn
+		trip.append([x2,y2])
+
+	else:
+		trip.append
+		
+
+a = math.pow(x2 - x1)
+		b = math.pow(y2 - y1)
+		distance = math.sqrt(a + b)
+
+
+
+
+
+"""	
+distance = math.sqrt()
+	#leftTurn (+slope)
+	if slope > 0
+
+	#rightTurn
+	else
+"""
--- skeli.py
+++ skeli.py
@@ -2,6 +2,7 @@
 #important imports
 from sys import stdin
 import math
+#//////////////////////////////
 
 #read array from stdin w/ spaces
 inputArray = []
@@ -13,18 +14,30 @@ for element in input().split():
 singleInputVal = input()
 #//////////////////////////////
 
-#set up row and col for matrix
-row = 4
-col = 4
+#/////for i range(start,end,step)////////////
+for i in range(start,-1,-1):
+#//////////////////////////////
 
-#for i range(start,end,step)
-for i in range(start,-1,-1):  
+#//Read .txt each line with 2 arguments on each line////////////
+from sys import stdin
+file = open('gardenIn.txt', 'r') # readFile()
+numberOfPoints = int(file.readline()) #cast for line to input
 
-#Create decoding matrix
+points =[]	# empty list
+for ele in range(0,numberOfPoints,1):
+	x,y = file.readline().strip().split() #fill list with 
+	points.append([int(x),int(y)])
+
+print(points) 
+#//////////////////////////////////////////////////////////
+
+
+#////Create decoding matrix///////////////////////////////
 Matrix = [[0 for x in range(row)] for x in range(col)]
 for i in range(col):
 	print("\n") # newline
 	for j in range(col):
 		sys.stdout.write(" " + "(%s,%s)" % (i,j)) #print matrix
 
-print("")	
+print("") #new line
+#//////////////////////////////////////////////////
