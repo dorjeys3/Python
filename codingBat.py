6c3902f6b5eb9620f5bc23eed37cefdf53957238
hamt3ch <hmiles23@ufl.edu>
2015-04-20 22:43:04 -0400

    New Euler, as well as CodingBat

--- codingBat.py
+++ codingBat.py
@@ -185,7 +185,7 @@ def front3(str):
 
 
 
-#List - 1 #####################################################
+#List - 1 ########################################################################################################
 """
 first_last6
 Given an array of ints, return True if 6 appears as either the first or last element in the array. The array will be length 1 or more. 
@@ -346,10 +346,43 @@ def has23(nums):
 	else:
 		return False
 
+#Logic - 1 ########################################################################################################
+"""
 
+When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper bound on the number of cigars. Return True if the party with the given values is successful, or False otherwise. 
 
+cigar_party(30, False) → False
+cigar_party(50, False) → True
+cigar_party(70, True) → True
+"""
+def cigar_party(cigars,is_weekend):
+	if(is_weekend and cigars >= 40):
+		return True
+	elif(cigars >= 40 and cigars <= 60):
+		return True
+	else:
+		return False
 
+"""
 
+You and your date are trying to get a table at a restaurant. The parameter "you" is the stylishness of your clothes, in the range 0..10, and "date" is the stylishness of your date's clothes.
+The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes. If either of you is very stylish, 8 or more, then the result is 2 (yes). With the exception that if either 
+of you has style of 2 or less, then the result is 0 (no). Otherwise the result is 1 (maybe). 
+
+date_fashion(5, 10) → 2
+date_fashion(5, 2) → 0
+date_fashion(5, 5) → 1
+
+"""
+
+def date_fashion(you,date):
+	if(you =< 2 or date =< 2):
+		return 0;
+	elif(you >= 8 or date >= 8):
+		return 2;
+	else:
+		return 1;
+	
 
 
 
